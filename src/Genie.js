aa90f87e42a7f19852e5049dc7530f7b37791508
Deanboy <jbehforouz@gmail.com>
2025-08-08 11:50:56 -0400

    Updates

--- src/Genie.js
+++
@@ -1,8 +0,0 @@
-import GeniePic from "./Images/TheGreatMeatball.png";
-
-export default function Genie( {spindir} ) {
-    const className = spindir;
-    return(
-        <img src={GeniePic} className={className} alt="logo"/>
-    );
-}
